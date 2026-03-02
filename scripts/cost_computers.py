Fixed L2 mean shift penalty bug, tested beta for mean and variance shift

scripts/cost_computers.py
@@ -1,25 +1,24 @@
 import numpy as np
 
-def get_gain_threshold(signal: np.ndarray, cost_type: str, horizon_size:int|None = None) -> float:
+def get_gain_threshold(signal: np.ndarray, cost_type: str, signal_var: float|None = None, horizon_size:int|None = None) -> float:
     n = len(signal)
     if n == 0:
         return 0
     # if horizon_size is not None:
     #     n = max(horizon_size, n)
     if cost_type == 'l2':
-        beta = 1.0
-        signal_var = np.var(signal)
+        beta = 2.7
         return beta * np.log(n) * signal_var
     if cost_type == 'normal':
-        beta = 3.0
+        beta = 1.3
         return beta * np.log(n)
     if cost_type == 'linear':
-        beta = 2.0
+        beta = 1.2
         diff_var = np.var(np.diff(signal))
         return (2 * beta) * np.log(n) * diff_var / 2
     if cost_type == 'mean_var':
-        beta = 1.0
-        return 2 * beta * np.log(n)
+        beta = 2.0
+        return beta * np.log(n)
     raise ValueError('Unsupported cost type')
 
 class CostComputer:

scripts/generate_cpd_gt.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import scipy
 
 def generate_cp_series(num_tests: int = 10, num_points: int = 10, cost_type:str = 'linear'):
-    methods = {"linear": generate_linear_cpd}
+    methods = {"linear": generate_linear_cpd, 'normal': generate_variance_cpd, 'l2': generate_mean_cpd}
     try:
         cur_method = methods[cost_type]
     except KeyError:
@@ -29,7 +29,7 @@ def generate_linear_cpd(num_points: int = 10):
     cur_point = init_length
     for _ in range(num_points):
         cur_slope = np.random.uniform(low=slope_range[0], high=slope_range[1])
-        while np.abs(prev_slope-cur_slope)<0.3:
+        while np.abs(prev_slope-cur_slope)<0.35:
             cur_slope = np.random.uniform(low=slope_range[0], high=slope_range[1])
         cur_intercept = time_series[-1][-1]
         cur_length = np.random.randint(low=length_range[0], high=length_range[1]+1)
@@ -43,6 +43,55 @@ def generate_linear_cpd(num_points: int = 10):
         cur_point += cur_length
         prev_slope = cur_slope
     return np.concatenate(time_series), change_points
+
+def generate_variance_cpd(num_points: int = 10):
+    std_list = [1.0]
+    for i in range(num_points):
+        std = np.random.uniform(0.3, 5.0)
+        while std / std_list[-1] < 1.7 and std_list[-1] / std < 1.7:
+            std = np.random.uniform(0.5, 7.0)
+        std_list.append(std)
+    # print(std_list)
+    time_series = []
+    cps = []
+    last_point = 0
+    for i in range(len(std_list)):
+        cur_size = np.random.randint(low=50, high=80)
+        segment = np.random.normal(0, std_list[i], size=cur_size)
+        time_series.append(segment)
+        last_point += cur_size
+        cps.append(last_point)
+    cps.pop()
+    return np.concatenate(time_series), cps
+
+def generate_mean_cpd(num_points: int = 10):
+    base_mean = 10
+    mean_list = [base_mean]
+    std = base_mean * 0.2
+    last_point = 25 
+    drift_size = 8
+    cps = []
+    time_series = [np.random.normal(mean_list[0], std, size=last_point)]
+    for i in range(num_points):
+        factor_down = np.random.uniform(-0.5, -0.9)
+        factor_up = np.random.uniform(0.5, 0.9)
+        factor = np.random.choice([factor_up, factor_down])
+        new_mean = mean_list[-1] + base_mean * factor
+        # std = base_mean * 0.2
+        if new_mean < mean_list[-1]:
+            drift = np.linspace(new_mean, mean_list[-1], num=drift_size, endpoint=True)[::-1]
+        else:
+            drift = np.linspace(mean_list[-1], new_mean, num=drift_size, endpoint=True)
+        time_series.append(drift + np.random.normal(std))
+        cps.append(last_point + drift_size//2+1)
+        new_size = np.random.randint(30, 60)
+        mean_list.append(new_mean)
+        segment = np.random.normal(new_mean, std, size=new_size)
+        time_series.append(segment)
+        last_point += drift_size + new_size 
+    return np.concatenate(time_series), cps
+
+    
 
 def plot_cps(time_series: np.ndarray, change_points: list[int]):
     plt.figure(figsize=(12, 6))
@@ -77,6 +126,6 @@ def plot_cps_with_detections(time_series: np.ndarray, change_points: list[int], 
     plt.show()
 
 if __name__ == "__main__":
-    series, cps = generate_cp_series(num_tests=1, num_points=10, cost_type='linear')
-    plot_cps(series, cps)
+    tests = generate_cp_series(num_tests=1, num_points=10, cost_type='linear')
+    plot_cps(tests[0][0], tests[0][1])
 

scripts/online_cost_computers.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class OnlineCostComputer:
-    def __init__(self, cost_type: str, horizon_size:int = 100):
+    def __init__(self, cost_type: str, horizon_size:int = 100, signal_var: float|None = None):
         self.horizon_size = horizon_size
         self.cost_type = cost_type
+        self.signal_var = signal_var
         self.cum_sum = np.array([0])
         self.cum_sum_sq = np.array([0])
         self.n_samples = 0
@@ -47,14 +48,17 @@ class OnlineCostComputer:
             return 0
         n = self.horizon_size
         if self.cost_type == 'normal':
-            beta = 1.7
+            beta = 5.0
             return beta * np.log(n)
         if self.cost_type == 'mean_var':
             beta = 2.0
             return 2 * beta * np.log(n)
-        _, variance = self._get_stats(start, end)
+        if self.signal_var is not None:
+            variance = self.signal_var
+        else:
+            _, variance = self._get_stats(start, end)
         if self.cost_type == 'l2':
-            beta = 1.0
+            beta = 4.0
             return beta * np.log(n) * variance
         
         raise ValueError('Unsupported cost type')

scripts/online_detector.py
@@ -3,7 +3,7 @@ from bin_seg import BinarySegmentation
 from opt_seg import OptSegmentation, OnlineOptSegmentation 
 
 class NaiveOnlineDetector:
-    def __init__(self, cost_type:str, model_type:str = 'opt', min_dist: int = 5, horizon_size:int=30):
+    def __init__(self, cost_type:str, model_type:str = 'opt', min_dist: int = 5, horizon_size:int=30, signal_var: float|None = None):
         if model_type == 'opt':
             self.model = OptSegmentation(cost_type, min_dist)
         elif model_type == 'binseg':
@@ -16,14 +16,14 @@ class NaiveOnlineDetector:
         self.horizon_size = horizon_size
         self.n_samples = 0
         self.offset = 0
-
+        self.signal_var = signal_var
 
     def update(self, value) -> bool:
         self.n_samples += 1
         self.signal_buffer.append(value)
         if len(self.signal_buffer) < 2 * self.min_dist:
             return False 
-        change_points = self.model.fit_predict(np.array(self.signal_buffer), horizon_size=self.horizon_size)
+        change_points = self.model.fit_predict(np.array(self.signal_buffer), horizon_size=self.horizon_size, signal_var=self.signal_var)
         if len(change_points) == 0:
             return False
         
@@ -38,8 +38,8 @@ class NaiveOnlineDetector:
 
 
 class FastOnlineDetector:
-    def __init__(self, cost_type:str, min_dist: int = 5, horizon_size:int=60):
-        self.model = OnlineOptSegmentation(cost_type, min_dist, horizon_size=horizon_size)
+    def __init__(self, cost_type:str, min_dist: int = 5, horizon_size:int=60, signal_var: float|None = None):
+        self.model = OnlineOptSegmentation(cost_type, min_dist, horizon_size=horizon_size, signal_var=signal_var)
         # self.signal_buffer = []
         self.min_dist = min_dist
         self.offset: int = 0

scripts/opt_seg.py
@@ -8,11 +8,11 @@ class OptSegmentation:
         self.min_dist = min_dist
         self.cost_computer = None
 
-    def fit_predict(self, signal: np.ndarray, horizon_size:int|None=None) -> list[int]:
+    def fit_predict(self, signal: np.ndarray, horizon_size:int|None=None, signal_var: float|None=None) -> list[int]:
         n = len(signal)
         if n < 2 * self.min_dist:
             return []
-        min_gain = get_gain_threshold(signal, self.model, horizon_size=horizon_size)
+        min_gain = get_gain_threshold(signal, self.model, signal_var=signal_var, horizon_size=horizon_size)
         if self.model == 'linear':
             self.cost_computer = LinearCostComputer(signal)
         else:
@@ -54,13 +54,13 @@ class OptSegmentation:
     
 
 class OnlineOptSegmentation:
-    def __init__(self, cost_type = 'normal', min_dist: int = 5, horizon_size: int = 60):
+    def __init__(self, cost_type = 'normal', min_dist: int = 5, horizon_size: int = 60, signal_var: float|None = None):
         self.cost_type = cost_type
         self.min_dist = min_dist
         if cost_type == 'linear':
             self.cost_computer = LinearOnlineCostComputer(horizon_size=horizon_size)
         else:
-            self.cost_computer = OnlineCostComputer(cost_type, horizon_size=horizon_size)
+            self.cost_computer = OnlineCostComputer(cost_type, horizon_size=horizon_size, signal_var=signal_var)
         self.horizon_size = horizon_size
         self.n_samples = 0 
         self.C = np.full(2*self.min_dist+1, np.inf)
@@ -86,6 +86,7 @@ class OnlineOptSegmentation:
             start_idx = self.n_samples
             if self.C.shape[0] <= self.n_samples:
                 self.double_size()
+        # this loop has mostly 1 iteration, except initial phase
         for t in range(start_idx, self.n_samples+1):
             self.candidates_set.add(t-self.min_dist)
             if t == self.n_samples:
@@ -109,7 +110,7 @@ class OnlineOptSegmentation:
             self.C[t] = min_cost
             self.path[t] = best_point
             for p in prev_points:
-                if self.C[p] + self.cost_computer.cost(p, t) > self.C[t]:
+                if self.C[p] + self.cost_computer.cost(p, t) + penalty > self.C[t]:
                     self.candidates_set.discard(p)
         return self.backtrack_changepoints()
 

scripts/run_test.py
@@ -136,9 +136,11 @@ def test_detection(time_series: np.ndarray | None = None, time_axes = None, cost
     else:
         change_points = []
         # model = NaiveOnlineDetector(cost_type=cost_type, min_dist=15, horizon_size=100, model_type=model_type)
-        model = FastOnlineDetector(cost_type='linear', min_dist=15, horizon_size=100)
+        
+        signal_var = 1.0 if cost_type == 'l2' else None
+        min_dist = 21 if cost_type == 'normal' else 15
+        model = FastOnlineDetector(cost_type=cost_type, min_dist=min_dist, horizon_size=100, signal_var=signal_var)
         n = len(time_series)
-        print(n)
         detection_times = []
         for i in range(n):
             is_detected = model.update(time_series[i])
@@ -152,13 +154,13 @@ def test_detection(time_series: np.ndarray | None = None, time_axes = None, cost
     
 
 def run_tests():
-    # test_detection(None, None, cost_type='linear', model_type='opt', method='online', plot=True)
-    jobs_data = prepare_jobs_data()
+    test_detection(None, None, cost_type='normal', model_type='opt', method='online', plot=True)
+    # jobs_data = prepare_jobs_data()
     target_col = 'Total_jobs'
     # cost_type = 'mean_var'
     cost_type = 'linear' 
     
-    test_detection(time_series=jobs_data[target_col].values, time_axes=jobs_data.index, cost_type=cost_type, model_type='opt', method='online')
+    # test_detection(time_series=jobs_data[target_col].values, time_axes=jobs_data.index, cost_type=cost_type, model_type='opt', method='online')
     
     # stock_data = load_data(ticker='^GSPC', start_date=datetime.datetime(2001, 1, 1), interval='1mo')
     # stock_data = prepare_data(stock_data)