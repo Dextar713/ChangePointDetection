Fixed float overflow bug for LinearOnlineCostComputer

scripts/cost_computers.py
@@ -13,11 +13,12 @@ def get_gain_threshold(signal: np.ndarray, cost_type: str, signal_var: float|Non
         beta = 1.3
         return beta * np.log(n)
     if cost_type == 'linear':
-        beta = 1.2
+        # beta = 1.7
+        beta = 3.0
         diff_var = np.var(np.diff(signal))
         return (2 * beta) * np.log(n) * diff_var / 2
     if cost_type == 'mean_var':
-        beta = 2.0
+        beta = 1.5
         return beta * np.log(n)
     raise ValueError('Unsupported cost type')
 

scripts/generate_cpd_gt.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import scipy
 
 def generate_cp_series(num_tests: int = 10, num_points: int = 10, cost_type:str = 'linear'):
-    methods = {"linear": generate_linear_cpd, 'normal': generate_variance_cpd, 'l2': generate_mean_cpd}
+    methods = {"linear": generate_linear_cpd, 'normal': generate_variance_cpd, 
+               'l2': generate_mean_cpd, 'mean_var': generate_variance_cpd}
     try:
         cur_method = methods[cost_type]
     except KeyError:
@@ -18,9 +19,10 @@ def generate_cp_series(num_tests: int = 10, num_points: int = 10, cost_type:str 
 
 
 def generate_linear_cpd(num_points: int = 10):
-    slope_range = (-1.5, 1.5)
+    slope_range = (-1.7, 1.7)
     init_intercept_range = (20, 100)
     length_range = (20, 50)
+    # length_range = (20, 150)
     prev_slope = np.random.uniform(low=slope_range[0], high=slope_range[1])
     init_intercept = np.random.uniform(low=init_intercept_range[0], high=init_intercept_range[1])
     init_length = np.random.randint(low=length_range[0], high=length_range[1]+1)
@@ -29,7 +31,7 @@ def generate_linear_cpd(num_points: int = 10):
     cur_point = init_length
     for _ in range(num_points):
         cur_slope = np.random.uniform(low=slope_range[0], high=slope_range[1])
-        while np.abs(prev_slope-cur_slope)<0.35:
+        while np.abs(prev_slope-cur_slope)<0.4:
             cur_slope = np.random.uniform(low=slope_range[0], high=slope_range[1])
         cur_intercept = time_series[-1][-1]
         cur_length = np.random.randint(low=length_range[0], high=length_range[1]+1)

scripts/online_cost_computers.py
@@ -51,7 +51,7 @@ class OnlineCostComputer:
             beta = 5.0
             return beta * np.log(n)
         if self.cost_type == 'mean_var':
-            beta = 2.0
+            beta = 2.2
             return 2 * beta * np.log(n)
         if self.signal_var is not None:
             variance = self.signal_var
@@ -75,8 +75,18 @@ class LinearOnlineCostComputer:
         
         self.sum_diff_var = np.array([0])
         self.n_samples = 0
+        self.offset = 0
 
     def double_size(self):
+        if self.n_samples >= 2*self.horizon_size:
+            self.offset = self.n_samples - self.horizon_size - 1
+            self.sum_x[self.offset:] -= self.sum_x[self.offset]
+            self.sum_xx[self.offset:] -= self.sum_xx[self.offset]
+            self.sum_xy[self.offset:] -= self.sum_xy[self.offset]
+            self.sum_y[self.offset:] -= self.sum_y[self.offset]
+            self.sum_yy[self.offset:] -= self.sum_yy[self.offset]
+            self.sum_diff_var[self.offset:] -= self.sum_diff_var[self.offset]
+
         self.sum_x = np.concatenate([self.sum_x, np.zeros(self.n_samples+1)])
         self.sum_y = np.concatenate([self.sum_y, np.zeros(self.n_samples+1)])
         self.sum_xx = np.concatenate([self.sum_xx, np.zeros(self.n_samples+1)])
@@ -92,11 +102,12 @@ class LinearOnlineCostComputer:
         # print(self.n_samples)
         if cur_size <= self.n_samples:
             self.double_size()
-        self.sum_x[self.n_samples] = self.sum_x[self.n_samples-1] + self.n_samples
+        x_val = self.n_samples - self.offset
+        self.sum_x[self.n_samples] = self.sum_x[self.n_samples-1] + x_val
         self.sum_y[self.n_samples] = self.sum_y[self.n_samples-1] + val
-        self.sum_xx[self.n_samples] = self.sum_xx[self.n_samples-1] + self.n_samples**2
+        self.sum_xx[self.n_samples] = self.sum_xx[self.n_samples-1] + x_val**2
         self.sum_yy[self.n_samples] = self.sum_yy[self.n_samples-1] + val**2
-        self.sum_xy[self.n_samples] = self.sum_xy[self.n_samples-1] + self.n_samples*val 
+        self.sum_xy[self.n_samples] = self.sum_xy[self.n_samples-1] + x_val*val 
         if self.n_samples > 1:
             self.sum_diff_var[self.n_samples-1] = self.sum_diff_var[self.n_samples-2] + (val - self.sum_y[self.n_samples-1] + self.sum_y[self.n_samples-2])**2
 
@@ -122,7 +133,7 @@ class LinearOnlineCostComputer:
         n = end - start
         if n == 0:
             return 0
-        beta = 1.2
+        beta = 3.0
         diff_var = (self.sum_diff_var[end-1] - self.sum_diff_var[start])/(n-1)
         # print(diff_var)
         return beta * np.log(self.horizon_size) * diff_var

scripts/prepare_data.py
@@ -5,7 +5,7 @@ import numpy as np
 import yfinance as yf
 import pandas as pd
 from statsmodels.tsa.seasonal import seasonal_decompose
-
+# from dotenv import load_dotenv
 
 # class DataPrepare:
 #     def __init__(self):
@@ -52,6 +52,8 @@ def prepare_data(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 def prepare_jobs_data() -> pd.DataFrame:
+    # load_dotenv()
+    # fred_api_key = os.getenv('fred_api_key')
     fred_api_key = "cd54e1d9baa5154d79ca7ac54ab064e0"
     fred = Fred(api_key=fred_api_key)
     sectors = ["Total_jobs", "Finance", "Construction", "Education_Healthcare", "Leisure_Hospitality", "Manufacturing",
@@ -86,3 +88,31 @@ def prepare_jobs_data() -> pd.DataFrame:
     # sum1 = np.sum(jobs_data.iloc[0]) - 2*jobs_data['Total_jobs'].iloc[0]
     # print(sum1)
     return jobs_data
+
+def prepare_employment_data() -> pd.DataFrame:
+    # load_dotenv()
+    # fred_api_key = os.getenv('fred_api_key')
+    fred_api_key = "cd54e1d9baa5154d79ca7ac54ab064e0"
+    fred = Fred(api_key=fred_api_key)
+    sectors = ['TotalNonfarm', 'healthcare', 'architectural_engineering', 'construction', 'PROFESSIONAL_SCIENCE_TECHNICAL_SERVICES',
+               'ComputerElectronicProductManufacturing', 'ComputerSystemsDesign', 'FinanceInsurance', 'LeisureHospitality']
+    series_codes = ['PAYEMS', 'CES6562000101', 'CES6054130001', 'USCONS', 'CES6054000001',
+                    'CES3133400001', 'CES6054150001', 'CES5552000001', 'USLAH']
+    employment_data = pd.DataFrame()
+    for i in range(len(sectors)):
+        path_to_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', f'employment_{sectors[i]}.csv')
+        if not os.path.exists(path_to_csv):
+            print(f"Fetching {sectors[i]} Employment data...")
+            sector_data = fred.get_series(series_codes[i])
+            sector_data.to_csv(path_to_csv, header=False)
+        else:
+            print(f"{sectors[i]} Employment data already exists. Skipping download.")
+            sector_data = pd.read_csv(path_to_csv, index_col=0, parse_dates=True, header=None)
+        sector_data = sector_data.dropna()
+
+        employment_data = pd.concat([employment_data, sector_data], axis=1)
+    employment_data.columns = sectors
+    employment_data.index.name = 'Date'
+    employment_data = employment_data.dropna()[:-1]
+    # print(employment_data.head())
+    return employment_data

scripts/run_test.py
@@ -7,7 +7,7 @@ import numpy as np
 from bin_seg import BinarySegmentation
 from opt_seg import OptSegmentation
 from online_detector import NaiveOnlineDetector, FastOnlineDetector
-from prepare_data import load_data, prepare_data, prepare_jobs_data
+from prepare_data import load_data, prepare_data, prepare_jobs_data, prepare_employment_data
 from stats_tests import check_stationarity
 import warnings
 warnings.filterwarnings("ignore")
@@ -76,13 +76,13 @@ def visualize_detections(time_series: np.ndarray, time_axes, change_points: np.n
     plt.plot(time_axes, time_series, color='gray', alpha=0.6, label='Time series')
     plt.title("Detected Change Points ")
 
-    if detection_times is None:
-        for point in change_points:
-            if is_x_date:
-                x_val = time_axes[point-1]
-            else:
-                x_val = point
-            plt.axvline(x=x_val, color='red', linestyle='--', linewidth=1, label='Change Point')
+    # if detection_times is None:
+    for point in change_points:
+        if is_x_date:
+            x_val = time_axes[point-1]
+        else:
+            x_val = point
+        plt.axvline(x=x_val, color='red', linestyle='--', linewidth=1, label='Change Point')
 
     if detection_times is not None:
         for t in detection_times:
@@ -138,8 +138,8 @@ def test_detection(time_series: np.ndarray | None = None, time_axes = None, cost
         # model = NaiveOnlineDetector(cost_type=cost_type, min_dist=15, horizon_size=100, model_type=model_type)
         
         signal_var = 1.0 if cost_type == 'l2' else None
-        min_dist = 21 if cost_type == 'normal' else 15
-        model = FastOnlineDetector(cost_type=cost_type, min_dist=min_dist, horizon_size=100, signal_var=signal_var)
+        min_dist = 21 if cost_type == 'normal' else 18
+        model = FastOnlineDetector(cost_type=cost_type, min_dist=min_dist, horizon_size=150, signal_var=signal_var)
         n = len(time_series)
         detection_times = []
         for i in range(n):
@@ -154,14 +154,20 @@ def test_detection(time_series: np.ndarray | None = None, time_axes = None, cost
     
 
 def run_tests():
-    test_detection(None, None, cost_type='normal', model_type='opt', method='online', plot=True)
+    # test_detection(None, None, cost_type='linear', model_type='opt', method='online', plot=True)
+
     # jobs_data = prepare_jobs_data()
-    target_col = 'Total_jobs'
-    # cost_type = 'mean_var'
-    cost_type = 'linear' 
-    
+    # print(jobs_data.tail())
+    # target_col = 'Leisure_Hospitality'
+    # cost_type = 'linear' 
     # test_detection(time_series=jobs_data[target_col].values, time_axes=jobs_data.index, cost_type=cost_type, model_type='opt', method='online')
     
+    employment_data = prepare_employment_data()
+    target_col = 'ComputerSystemsDesign'
+    cost_type = 'linear'
+    employment_data[target_col] = (employment_data[target_col]-employment_data[target_col].min())/(employment_data[target_col].max()-employment_data[target_col].min())
+    test_detection(time_series=employment_data[target_col].values, time_axes=employment_data.index, cost_type=cost_type, model_type='opt', method='online')
+
     # stock_data = load_data(ticker='^GSPC', start_date=datetime.datetime(2001, 1, 1), interval='1mo')
     # stock_data = prepare_data(stock_data)
     # target_col = 'Close'
@@ -179,4 +185,9 @@ if __name__ == '__main__':
     run_tests()
     end_tm = time()
     duration = end_tm - start_tm
-    print('Duration: ', np.round(duration, 2))
+    print('Duration (s): ', np.round(duration, 2))
+
+    # import torch
+    # print(torch.version)
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # print(device)